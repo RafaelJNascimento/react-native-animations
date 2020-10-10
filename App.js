6a14af3f38c22864824428492b586d6ed236769d
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-10-10 02:50:34 -0300

    09 - Animando Header - 'melhorado'

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {
-  View,
-  Platform,
-  Animated,
+  SafeAreaView,
   StyleSheet,
-  ScrollView,
   StatusBar,
-  Image,
-  Text
+  Animated,
 } from 'react-native';
 import User from './components/Users';
 import usersData from './domain/userData';
 
-const app = () => {
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = 70;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
-  const scrowOffset = useRef(new Animated.Value(0)).current;
+const App = () => {
+
   const [userSelected, SetUserSelected] = useState();
   const [userInfoVisible, SetUserInfoVisible] = useState();
   const [users] = useState([...usersData]);
@@ -24,82 +23,93 @@ const app = () => {
     SetUserInfoVisible(true);
   }
 
+  const scrollY = useRef(new Animated.Value(0)).current;
+
+  const headerTranslateY = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, -HEADER_SCROLL_DISTANCE],
+    extrapolate: 'clamp',
+  });
+
+  const imageTranslateY = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, 100],
+    extrapolate: 'clamp',
+  });
+
+  const scale = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [1, 0.7],
+    extrapolate: 'clamp',
+  });
+
   renderDetail = () => (
-    <View style={styles.container}>
-      <ScrollView>
-        <User
-          user={userSelected}
-          onPress={() => {
-            SetUserSelected(null);
-            SetUserInfoVisible(false)
-          }}
-        />
-      </ScrollView>
-    </View>
+    <Animated.ScrollView
+      contentContainerStyle={{ paddingTop: HEADER_MAX_HEIGHT }}
+      scrollEventThrottle={16}
+      onScroll={Animated.event([{
+        nativeEvent: { contentOffset: { y: scrollY } }
+      }],
+        { useNativeDriver: true, }
+      )}
+    >
+      <User
+        user={userSelected}
+        onPress={() => {
+          SetUserSelected(null);
+          SetUserInfoVisible(false)
+        }}
+      />
+    </Animated.ScrollView>
   )
 
   renderList = () => (
-    <View style={styles.container}>
-      <ScrollView
-        scrollEventThrottle={16}
-        onScroll={Animated.event([{
-          nativeEvent: {
-            contentOffset: { y: scrowOffset }
-          }
-        }],
-          {
-            useNativeDriver: false,
-            isInteraction: false
-          })}
-      >
-        {users.map(user =>
-          <User
-            key={user.id}
-            user={user}
-            onPress={() => selectUser(user)}
-          />
-        )}
-      </ScrollView>
-    </View>
+
+    <Animated.ScrollView
+      contentContainerStyle={{ paddingTop: HEADER_MAX_HEIGHT }}
+      scrollEventThrottle={16}
+      onScroll={Animated.event([{
+        nativeEvent: { contentOffset: { y: scrollY } }
+      }],
+        { useNativeDriver: true, }
+      )}
+    >
+      {users.map(user =>
+        <User
+          key={user.id}
+          user={user}
+          onPress={() => selectUser(user)}
+        />
+      )}
+    </Animated.ScrollView>
+
   )
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar
         barStyle="light-content"
         translucent
         backgroundColor="transparent" />
+      {userInfoVisible ? renderDetail() : renderList()}
       <Animated.View
         style={[
-          styles.header,
-          {
-            height: scrowOffset.interpolate({
-              inputRange: [0, 140],
-              outputRange: [200, 70],
-              extrapolate: 'clamp',
-            })
-          }
-        ]}
-      >
-        <Image
+          styles.header, { transform: [{ translateY: headerTranslateY }] }]}>
+        <Animated.Image
           source={userSelected ? { uri: userSelected.thumbnail } : null}
-          style={styles.headerImage}
+          style={[styles.headerImage, { transform: [{ translateY: imageTranslateY }], }]}
         />
-        <Animated.Text style={[styles.HeaderText,
-         {
-          fontSize: scrowOffset.interpolate({
-            inputRange: [120, 140],
-            outputRange: [24, 16],
-            extrapolate: 'clamp',
-          })
-        }
-      ]}>{userSelected ? userSelected.name : 'GoNative'}</Animated.Text>
+        <Animated.Text style={[styles.HeaderText, {
+          transform: [{
+            scale: scale
+          }]
+        }]}>
+          {userSelected ? userSelected.name : 'GoNative'}</Animated.Text>
       </Animated.View>
-
-      {userInfoVisible ? renderDetail() : renderList()}
-    </View>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -107,8 +117,13 @@ const styles = StyleSheet.create({
   },
   header: {
     paddingTop: Platform.OS === 'ios' ? 40 : 20,
-    paddingLeft: 15,
-    paddingRight: 15,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    overflow: 'hidden',
+    height: HEADER_MAX_HEIGHT,
     backgroundColor: '#2E93E5',
   },
   headerImage: {
@@ -117,6 +132,8 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     bottom: 0,
+    height: HEADER_MAX_HEIGHT,
+    resizeMode: 'cover',
   },
   HeaderText: {
     fontWeight: 'bold',
@@ -124,8 +141,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     position: 'absolute',
     left: 15,
-    bottom: 20,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+    fontSize: 25,
+    bottom: 8,
   }
 });
 
-export default app;
+export default App;
